d714a40c1ca65588e5fc7dc1eee57063cfa9bbd4
Sand1312 <87612288+Sand1312@users.noreply.github.com>
2022-09-23 20:21:54 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,30 +1,26 @@
 #include <iostream>
 #include "algorithm"
-#include "vector"
+#include "math.h"
 using namespace std;
-vector <int> b;
+int a[10000000]{};
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     int n,m;cin>>n>>m;
-    int a[500005]{};
-    for (int i=0;i<n;i++) {
-        cin>>a[i];
-        b.push_back(a[i]);
-    }
-    sort(b.begin(),b.end());
-    for (int j=1;j<=log(b[n-1])/log(2)+1;j++) {
+    for (int i=0;i<n;i++) cin>>a[i];
+    sort(a,a+n);
+    for (int j=1;j<=log(a[n-1])/log(2)+1;j++) {
         for (int i = 0; i < n; i++) {
-            a[i]/=2;
-            b.push_back(a[i]);
+            a[i + j * n] = a[i + (j - 1) * n] / 2;
         }
     }
-    int ans{};
-    sort(b.begin(),b.end());
-    for (int i=b.size()-1;i>=0;i--) {
+    long long ans{};
+    int tmp=n*(log(a[n-1])/log(2)+1);
+    sort(a,a+tmp);
+    for (int i=tmp-1;i>=0;i--) {
         if (m>0) {
-            ans+=b[i];
+            ans+=a[i];
             m--;
         }
         else break;
