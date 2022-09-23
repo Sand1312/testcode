289ba5bb7aca40dec2c4d0c4e9de70c23cf4c8fa
Sand1312 <87612288+Sand1312@users.noreply.github.com>
2022-09-23 18:53:11 +0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,37 +1,33 @@
 #include <iostream>
 #include "algorithm"
-#include "math.h"
+#include "vector"
 using namespace std;
+vector <int> b;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     int n,m;cin>>n>>m;
     int a[500005]{};
-    for (int i=0;i<n;i++) cin>>a[i];
-    int ans{};
-    while (m>0) {
-        int index{},max{a[0]},mm{-99};
-        for (int i=1;i<n;i++) if (a[i]>max) {
-            max=a[i];
-            index=i;
+    for (int i=0;i<n;i++) {
+        cin>>a[i];
+        b.push_back(a[i]);
+    }
+    sort(b.begin(),b.end());
+    for (int j=1;j<=log(b[n-1])/log(2)+1;j++) {
+        for (int i = 0; i < n; i++) {
+            a[i]/=2;
+            b.push_back(a[i]);
         }
-        for (int i=0;i<n;i++) if (a[i]>=mm&&i!=index) mm=a[i];
-            if (max==0) break;
-            int tmp{};
-            if (mm!=0) tmp=log(max/mm)/log(2);
-            else tmp=log(max)/log(2);
-            if (m<tmp) tmp=m;
-            m-=tmp;
-            if (tmp==0) {
-                ans+=a[index];
-                a[index]/=2;
-                m--;
-            }
-            for (int j=0;j<tmp;j++) {
-                ans += a[index];
-                a[index] /= 2;
-            }
-}
+    }
+    int ans{};
+    sort(b.begin(),b.end());
+    for (int i=b.size()-1;i>=0;i--) {
+        if (m>0) {
+            ans+=b[i];
+            m--;
+        }
+        else break;
+    }
     cout<<ans;
 }
